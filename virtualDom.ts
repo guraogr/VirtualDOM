b646b1488266aaf49a0b09a258e21f9fa5ecd46d
guraogr <guraogr@gmail.com>
2022-06-24 03:08:28 +0900

    feat: h関数を実装

--- virtualDom.ts
+++ virtualDom.ts
@@ -34,3 +34,60 @@ interface VirtualNodeType {
   nodeType: TEXT_NODE | null; // このVNodeのタイプ(文字を表すノードなのか要素を表すノードなのか)
   key: KeyAttribute | null; // keyを表す
 }
+
+// ヘルパー関数
+
+// VNodeを作成する関数
+
+const TEXT_NODE = 3;
+
+const createVNode = (
+  name: VirtualNodeType["name"],
+  props: VirtualNodeType["props"],
+  children: VirtualNodeType["children"],
+  realNode?: VirtualNodeType["realNode"],
+  nodeType?: VirtualNodeType["nodeType"],
+  key?: KeyAttribute
+): VirtualNodeType => ({
+  name,
+  props,
+  children,
+  realNode: realNode === undefined ? null : realNode,
+  nodeType: nodeType === undefined ? null : nodeType,
+  key: key === undefined ? null : key,
+});
+
+const createTextVNode = (
+  name: string,
+  realNode?: VirtualNodeType["realNode"]
+) => {
+  return createVNode(name, {}, [], realNode, TEXT_NODE);
+};
+
+export const h = (
+  name: VirtualNodeType["name"],
+  props: VirtualNodeType["props"],
+  children: (VirtualNodeType | string)[],
+  realNode?: VirtualNodeType["realNode"]
+) => {
+  const VNodeChildren: VirtualNodeType[] = [];
+  for (const child of children) {
+    if (typeof child === "string") {
+      const textVNode = createTextVNode(child);
+      VNodeChildren.push(textVNode);
+    } else {
+      VNodeChildren.push(child);
+    }
+  }
+
+  const thisVNode = createVNode(
+    name,
+    props,
+    VNodeChildren,
+    realNode,
+    null,
+    props.key
+  );
+
+  return thisVNode;
+};
